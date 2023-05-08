bf1ac7c2312101cf1f7e993d9454178c9cff81fa
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-08 23:59:34 +0500

    now it's hopefully standardize

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: [process.env.FRONTEND_URL],
+        // origin: [process.env.FRONTEND_URL], 
+        origin: process.env.FRONTEND_URL,
         // origin: 'http://localhost:3000',
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
