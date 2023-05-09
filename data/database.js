e84d390e1a2541b6e62d697503c1ce7261721a83
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-09 06:25:02 +0500

    connectDB programmed differently

--- data/database.js
+++ data/database.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 
 export const connectDB = () => {
-    mongoose
-        .connect(process.env.MONGO_URI, {
-            dbName: "backendapi",
-        })
-        .then((c) => console.log(`Database Connected with ${c.connection.host}`))
-        .catch((e) => console.log(e));
+    // mongoose
+    //     .connect(process.env.MONGO_URI, {
+    //         dbName: "backendapi",
+    //     })
+    //     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
+    //     .catch((e) => console.log(e));
+
+    mongoose.connect(
+        process.env.MONGO_URL, { dbName: "backendapi1" },
+        { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+        () => {
+            console.log('Connected to MongoDB');
+        }
+    );
 };
