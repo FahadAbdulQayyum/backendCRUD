bde729f0983c446bff3dbdb62aac3ed174aa7461
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-09 06:44:14 +0500

    precess.env.PORT was written as only PORT, this was a forgotten mistake

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 //         console.log(e)
 //     })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(
         `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
     );
