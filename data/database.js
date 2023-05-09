ceaf16d3638f929d47e70a08344bd2f8dbfe77cc
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-09 06:35:53 +0500

    undone all changed

--- data/database.js
+++ data/database.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 
 export const connectDB = () => {
-    // mongoose
-    //     .connect(process.env.MONGO_URI, {
-    //         dbName: "backendapi",
-    //     })
-    //     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
-    //     .catch((e) => console.log(e));
+    mongoose
+        .connect(process.env.MONGO_URI, {
+            dbName: "backendapi",
+        })
+        .then((c) => console.log(`Database Connected with ${c.connection.host}`))
+        .catch((e) => console.log(e));
 
-    mongoose.connect(
-        process.env.MONGO_URI, { dbName: "backendapi1" },
-        { useNewUrlParser: true, useUnifiedTopology: true },
-        () => {
-            console.log('Connected to MongoDB');
-        }
-    );
+    // mongoose.connect(
+    //     process.env.MONGO_URI, { dbName: "backendapi1" },
+    //     { useNewUrlParser: true, useUnifiedTopology: true },
+    //     () => {
+    //         console.log('Connected to MongoDB');
+    //     }
+    // );
 };
