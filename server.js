d0fb53e25f5dd92a990acbd4cbd0d916b07c6c5a
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-09 06:18:01 +0500

    PORT sitting and standard changed

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import { app } from "./app.js";
 import { connectDB } from "./data/database.js";
+const PORT = process.env.PORT || 4000
 
 connectDB();
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log(
         `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
     );
